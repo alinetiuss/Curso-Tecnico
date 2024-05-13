c50eb34f40ab8333c83b6db5dd9424f54114dde5
AlineTiuss <169068304+alinetiuss@users.noreply.github.com>
2024-05-13 23:10:56 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("          Curso Técnico de Desenvolvimento de Sistemas.");
+    System.out.println("");
+    System.out.println("    O curso de Desenvolvimento de Sistemas, como o nome já diz, é algo relacionado a computação. São matérias novas que ensinam a como desenvolver projetos e até mesmo a consertar aparelhos eletrônicos.");
+    System.out.println("   Isso ajuda os alunos a se aprofundar e ter mais chances de vaga de emprego já que, atualmente, maior parte tem relação com a tecnologia. Exemplo: marketing digital, programação, técnico em eletrônca, técnico de informática, designer digital e muito mais. Apesar de muitos dos estudantes não gostarem dessa mudança, é uma aprendizagem interessante e que pode ser utilizada constantemente no futuro.");
+    System.out.println("    Depois dessa mudança repentina, a maioria dos alunos ficaram cansados de tantas tarefas e responsabilidades que começaram a ter, as dificuldades aumentaram nos estudos, principalmente, para aqueles que não tem acesso a internet ou a aparelhos eletrônicos, o que acabou gerando muita desistência em dar continuidade aos estudos. Não apenas isso, mas a ansiedade de passar no curso, aqueles que tem mais dificuldade no aprendizado, são obrigados a ficar no período noturno ou mudar de colégio, o que pode acabar afetando o psicológico do adolescente em pensar 'será que não sou bom o suficiente para passar?'");
+  System.out.println("    Como aluna desse curso, confesso que certos momentos fica bem cansativo com muitas plataformas para acessar e mais os cursos de programação. Particularmmente, essa não é a área que quero para mim, na realidade, não gosto de assuntos sobre computação e tecnologia, porém sei de sua importância. Gosto muito de matérias de exatas, então imaginei que me daria melhor nesse curso do que a Administração.");
+  }
+}
+  
